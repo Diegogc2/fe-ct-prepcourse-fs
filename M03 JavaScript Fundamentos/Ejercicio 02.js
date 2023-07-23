e1c252bbf2b69f8a016e39aafb19a80b6330f378
diegogcd2 <diegocajelli@gmail.com>
2023-07-23 16:45:18 -0300

    Resolucion de los ejercicios del 1 al 5

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,7 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string;
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,26 +12,36 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   var resultadoDeSuma = x + y;
+   return resultadoDeSuma;
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
-}
+   var resultadoDeResta = x - y;
+   return resultadoDeResta;
+} 
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   var resultadoDivide = x / y;
+   return resultadoDivide;
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   var resultadoMulti = (x * y);
+   return resultadoMulti;
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   var obtenerResto = (x % y);
+   return obtenerResto;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
